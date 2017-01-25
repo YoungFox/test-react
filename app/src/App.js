87f7c9d689b17d4c46efce6d80a53059f3a899fe
yangwenliang <yang6233562@gmail.com>
2017-01-25 15:58:15 +0800

    clock

--- app/src/App.js
+++ app/src/App.js
@@ -9,12 +9,19 @@ function testJs(obj){
 const obj={
   name: 'ywl'
 }
+
+let abs = (
+  <h1 className="hello">
+    Hello world!
+  </h1>
+);
 class App extends Component {
   render() {
     return (
       <div className="App" data-auther={testJs(obj)}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          {abs}
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,56 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+function Test(props){
+	return (<div>{props.name} 你好啊</div>);
+}
+
+class Clock extends React.Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {date: new Date()};
+	}
+
+	componentDidMount(){
+		this.timerId = setInterval(() => {
+			this.click();
+		},1000);
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.timerId);
+	}
+
+	render (){
+		return (
+			<section>
+			<div>{this.props.name}</div>
+			<div>{this.state.date.toLocaleTimeString()}</div>
+			</section>
+		)
+	}
+
+	click(){
+		this.setState({
+			date: new Date()
+		});
+	}
+} 
+
+
 ReactDOM.render(
-  <App />,
+	<div>
+	  <App >
+	  </App>
+
+	  <Test name="ylw" />
+	  <Test name="yang" />
+	  <Test name="wen" />
+	  <Test name="liang" />
+	  <Clock name="clock"/>
+  	</div>,
   document.getElementById('root')
 );
+
